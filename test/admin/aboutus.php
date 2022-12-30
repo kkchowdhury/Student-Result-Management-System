400165d8fc35d36ef88980195ea3c6c1fca11115
KISHALOY KAR CHOWDHURY <107786775+kkchowdhury@users.noreply.github.com>
2022-12-30 14:12:10 +0530

    Add files via upload

---
+++ test/admin/aboutus.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>About</title>
+<link rel="stylesheet" href="../csss/aboutus.css" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+
+</head>
+<body>
+    <header>
+      <nav>
+        <div class="row clearfix">
+            <ul class="main-nav" animate slideInDown>
+                <li><a href="../index.php">HOME</a></li>
+                <li><a href="aboutus.php">ABOUT</a></li>
+                <li><a href="contactus.php">CONTACT</a></li>
+                <li><a href="../login.php">ADMIN LOGIN</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="main-content-header">
+          <h2 class="search">About Us</h2>
+             <div class="div2">
+             <h3>Student Result Management System</h2>
+             <p>This software application provides the administration with a safe database system for keeping, analysing, and publishing the exam results  of candidates online. It also miraculously simplifies and accelerates the result management system with distinctive templates. The database also enables the student to view and look up the exam results online whenever necessary.
+              This System is a technological opportunity for the school, college university and coaching centre institutions searching for a secure, simple and alternative solution to the conventional paper-based exam results  evaluation, reporting and distribution.</p>
+             </div>  
+          </div>
+    </header>
+    
+</body>
+</html>
